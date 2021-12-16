2ce520a1796beadc82be8545afbf4b91e68575fa
Yusuf <yusufmucahitsolmaz@eskisehir.edu.tr>
2021-12-16 12:20:30 +0300

    Updating enum.

--- src/Months.java
+++ src/Months.java
@@ -22,6 +22,25 @@ public enum Months {
 
     November,
 
-    December,
+    December;
 
+    @Override
+    public String toString() {
+        switch (this){
+            case May : return "May";
+            case July: return "July";
+            case June: return "June";
+            case April: return "April";
+            case March: return "March";
+            case August: return "August";
+            case January: return "January";
+            case October: return "October";
+            case December: return "December";
+            case February: return "February";
+            case November: return "November";
+            case September: return "September";
+            default:return "Unspecified";
+        }
+    }
 }
+
